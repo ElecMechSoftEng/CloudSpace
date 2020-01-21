3bcb5a9f592107737a0723995aeb8750a9183f99
creighton.cloud <creighton.cloud@gmail.com>
2020-01-21 12:33:37 -0500

    Messing Around with changing the from text by command, but I does not seem to work properly yet. I'm going to try and follow a video to see what I'm missing. It is probably something simiple.

--- GameOfLife/GameOfLife/Form1.vb
+++ GameOfLife/GameOfLife/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
 
+
+    Public Sub FormSettings()
+        Form1.ActiveForm.BackColor = Color.Black
+        Form1.ActiveForm.Text = "Hello World"
+
+
+
+    End Sub
+    Public Sub DrawBoard()
+        Dim myPen As New System.Drawing.Pen(System.Drawing.Color.Red)
+        Dim formGraphics As System.Drawing.Graphics
+        formGraphics = Me.CreateGraphics()
+        formGraphics.DrawLine(myPen, 0, 0, 200, 200)
+        myPen.Dispose()
+        formGraphics.Dispose()
+    End Sub
 End Class
---
+++ GameOfLife/GameOfLife/Main.vb
@@ -0,0 +1,15 @@
+﻿Module Main
+    Public Sub Main()
+
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1)
+
+        Console.WriteLine("hello world")
+
+        'Form1.FormSettings()
+
+        'Form1.DrawBoard()
+
+    End Sub
+End Module
